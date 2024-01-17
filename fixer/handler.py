7d9b576be68922d88aa6e993cd96aa547cabee3a
Mohammad <mohammadmns1379@gmail.com>
2024-01-17 12:01:22 +0330

    initial commit

---
+++ fixer/handler.py
@@ -0,0 +1,2 @@
+def get_rates():
+    print('Package Test')
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="fixer-demo",
+       version='0.1', 
+       description='Fixer service demo package', 
+       url="https://github.com/MMansouri79/fixer-demo", 
+       author='Mohammad', 
+       author_email="mohammadmns1379@gmail.com",
+       license='MIT', 
+       packages=['fixer'], 
+       zip_safe=False
+    ) 
