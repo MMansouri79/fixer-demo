ffbad69d142130f33d30d972acc70b8907bc857a
Mohammad <mohammadmns1379@gmail.com>
2024-01-17 12:21:58 +0330

    bumo into v0.2

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,11 @@
-def get_rates():
-    print('Package Test')
+import requests
+import json
+
+
+BASE_PATH = "http://data.fixer.io/api/latest?access_key="
+
+def get_rate(api_key):
+    response = requests.get(BASE_PATH + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name="fixer-demo",
-       version='0.1', 
+       version='0.2', 
        description='Fixer service demo package', 
        url="https://github.com/MMansouri79/fixer-demo", 
        author='Mohammad', 
        author_email="mohammadmns1379@gmail.com",
        license='MIT', 
-       packages=['fixer'], 
+       packages=['fixer'],
+       install_requires=['requests'], 
        zip_safe=False
     ) 
